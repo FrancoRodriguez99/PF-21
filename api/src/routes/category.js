b9feacae4391ae8cb755bc7a56d9f0f5d32d1369
Franco <juanfrancorodriguez99@gmail.com>
2022-11-22 17:27:50 -0300

    create new items

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { getAll, getOne, createItem } = require("../controllers/articulosController.js");
+const { getAll, getOne } = require("../controllers/categoryController.js");
 
 const router = express.Router();
 
 router.get("/getAll", getAll);
 router.get("/:id", getOne);
-router.post("/createItem", createItem);
 
 module.exports = router;
